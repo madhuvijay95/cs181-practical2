142dc2ddca06cae7fd691221516b5efbcb2ffd27
Tyler VanValkenburg <victorclaude@Tylers-MacBook-Pro.local>
2016-03-04 04:31:39 -0500

    added new words and trimmed the code; metrics unchanged

--- lookfor.py
+++ lookfor.py
@@ -1,8 +1,8 @@
-## Counts specified words in training files
+## Counts specified words in training files.
 ## Reports their average occurance, standard deviation, likelihood of absence,
-## and average ignoring absences within each class - all divided a fraction of the total line
-## count so as to lessen file length's effect on the results
-## Assumes to be in a directory along with the training set directory labeled "train"
+## and average ignoring absences within each class - all divided by a fraction of the total line
+## count so as to lessen the file length's effect on the results.
+## Assumes to be in a directory along with the training set directory labeled "train".
 
 import numpy as np
 from os import listdir
@@ -11,26 +11,18 @@ from os.path import isfile, join
 classes = ["Agent", "AutoRun", "FraudLoad", "FraudPack", "Hupigon", "Krap",
            "Lipler", "Magania", "None", "Poison", "Swizzor", "Tdss",
            "VB", "Virut", "Zbot"]
-
-onlynames = [f for f in listdir('train') if isfile(join('train', f))]
-
+filenames = [f for f in listdir('train') if isfile(join('train', f))]
+words = ['socket', 'destroy_window', 'set_value', 'delete_value', 'targetpid', 'get_file_attributes',
+         'FILE_ANY_ACCESS', 'FILE_ATTRIBUTE_NORMAL', 'SECURITY_ANONYMOUS', 'import', 'export']
 data = {}
 
 for type in classes:
 
     data[type] = {}
-    data[type]['socket'] = []
-    data[type]['destroy_window'] = []
-    data[type]['set_value'] = []
-    data[type]['delete_value'] = []
-    data[type]['targetpid'] = []
-    data[type]['get_file_attributes'] = []
-    data[type]['FILE_ANY_ACCESS'] = []
-    data[type]['FILE_ATTRIBUTE_NORMAL'] = []
-    data[type]['SECURITY_ANONYMOUS'] = []
+    for word in words:
+        data[type][word] = []
 
-
-    for filename in onlynames:
+    for filename in filenames:
 
         if filename.split('.')[-2] == type:
 
@@ -40,20 +32,12 @@ for type in classes:
             length = len(text.split("\n"))/1000.0
             length = 1
 
-            data[type]['socket'].append(text.count("socket")/length)
-            data[type]['destroy_window'].append(text.count("destroy_window")/length)
-            data[type]['set_value'].append(text.count("set_value")/length)
-            data[type]['delete_value'].append(text.count("delete_value")/length)
-            data[type]['targetpid'].append(text.count("targetpid")/length)
-            data[type]['get_file_attributes'].append(text.count("get_file_attributes")/length)
-            data[type]['FILE_ANY_ACCESS'].append(text.count("FILE_ANY_ACCESS")/length)
-            data[type]['FILE_ATTRIBUTE_NORMAL'].append(text.count("FILE_ATTRIBUTE_NORMAL")/length)
-            data[type]['SECURITY_ANONYMOUS'].append(text.count("SECURITY_ANONYMOUS")/length)
-
+            for word in words:
+                data[type][word].append(text.count(word)/length)
 
 for set in data['Agent']:
     if set != 'length':
-        print '\n' + set + '\n'
+        print '\n **' + set + '** \n'
         for type in classes:
             list = data[type][set]
             zeroes = list.count(0)
