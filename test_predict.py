52458c89a38cc37972da9dcd8cf8641d5fb377a2
madhuvijay95 <madhuvijay95@gmail.com>
2016-03-04 15:56:52 -0500

    commenting

--- test_predict.py
+++ test_predict.py
@@ -3,16 +3,22 @@ import cPickle as pickle
 import csv
 import numpy as np
 
+# load data
 ids, X, t, used_features = pickle.load(open('data_matrix.p', 'r'))
+# load optimal classifier and feature set
 feature_mask, clf = pickle.load(open('classifier.p', 'r'))
 
+# extract test data
 X_test = X[t == -1][:, feature_mask]
 print X_test.shape
 ids_test = np.array(ids)[t == -1]
+# refit optimal model on train data
 clf.fit(X[t != -1][:, feature_mask], t[t != -1])
+# predict on test data
 predictions = clf.predict(X_test)
 predictions = zip(ids_test, predictions)
 
+# write to csv
 with open('predictions.csv', 'wb') as output_file:
     writer = csv.writer(output_file)
     writer.writerow(('Id', 'Prediction'))
